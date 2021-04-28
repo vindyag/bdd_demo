a1784b5ca1a096d76488edc488b5d703478bd130
vindyag <vindyag@99x.lk>
2021-04-28 18:38:01 +0530

    cleanup

--- src/main/java/demo/helper/ConfigurationLoader.java
+++ src/main/java/demo/helper/ConfigurationLoader.java
@@ -1,6 +1,5 @@
 package demo.helper;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
--- src/test/groovy/integration/demo/CucumberIntegrationTest.groovy
+++ src/test/groovy/integration/demo/CucumberIntegrationTest.groovy
@@ -5,6 +5,5 @@ import io.cucumber.junit.CucumberOptions
 import org.junit.runner.RunWith
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = ['src/test/resources/features'], glue = ['integration/steps'])
 class CucumberIntegrationTest {
 }

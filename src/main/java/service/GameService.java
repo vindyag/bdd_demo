e5cce7b9ca32fc6aea007be842b00b610c57d5a8
vindyag <vindyag@99x.lk>
2021-05-03 20:59:05 +0530

    add config

--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -1,11 +1,14 @@
 package service;
 
+import java.io.IOException;
+
 import service.fizzbuzz.FizzBuzz;
+import service.helper.ConfigurationLoader;
 
 public class GameService {
 
-    public Object[] play() {
-        int arraySize = 10;
+    public Object[] play() throws IOException {
+        int arraySize = ConfigurationLoader.getUpperLimit();
         FizzBuzz fizzBuzz = new FizzBuzz();
         Object[] result = new Object[arraySize];
         for (int i = 0; i < arraySize; i++) {
